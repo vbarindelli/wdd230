e4e4f6f5534b9ecd3f71e1b66f77ef65b6074d2a
vbarindelli <hae21002@byu.edu>
2024-11-20 11:20:56 +0100

    W02: Project Milestone

---
+++ chamber/scripts/index.js
@@ -0,0 +1,17 @@
+const year = document.querySelector("#currentyear");
+const today = new Date();
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+
+
+const lastModified = document.querySelector("#lastModified");
+let modified = new Date(document.lastModified);
+lastModified.innerHTML = `Last Modification: ${modified}</span>`;
+
+const mainnav = document.querySelector('.navigation');
+const hamburger = document.querySelector('#menu');
+
+hamburger.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hamburger.classList.toggle('show');
+})
