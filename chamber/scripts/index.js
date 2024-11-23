68cc7a39e19ef866f1d891ff971fb1554df65227
vbarindelli <hae21002@byu.edu>
2024-11-23 23:14:13 +0100

    W03: Project Milestone - The Chamber Discover Page

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -1,5 +1,6 @@
 const year = document.querySelector("#currentyear");
 const today = new Date();
+const msToDays = 86400000;
 year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 
 
@@ -15,3 +16,29 @@ hamburger.addEventListener('click', () => {
     mainnav.classList.toggle('show');
     hamburger.classList.toggle('show');
 })
+
+let welcomeMsg = "Welcome! Let us know if you have any questions.";
+lastVisitObj = window.localStorage.getItem("lastVisitDate");
+
+//TEST validations:
+//lastVisitObj = null;
+//lastVisitObj = new Date(Date.UTC(1, 1, 1));
+if (lastVisitObj) {
+    let lastVisitDate = new Date(Number(lastVisitObj));
+    let todayDate = Date.now();
+    let daysDiff = (todayDate - lastVisitDate) / msToDays;
+    if (daysDiff < 1) {
+        welcomeMsg = "Back so soon! Awesome!";
+    }
+    else {
+        welcomeMsg = `You last visited ${daysDiff.toFixed(0)} days ago.`;
+    }
+}
+
+const welcomeMsgElement = document.querySelector("#lastVisit");
+welcomeMsgElement.innerHTML = welcomeMsg;
+
+
+
+
+localStorage.setItem("lastVisitDate", Date.now());
